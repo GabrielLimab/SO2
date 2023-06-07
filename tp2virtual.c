19419e23a3d585c3203689ce7e6beaa1f9e1731f
Gabriel Teixeira <Gteixeiraca@gmail.com>
2023-06-07 11:03:46 -0300

    Feat: Argument parsing

--- tp2virtual.c
+++ tp2virtual.c
@@ -9,18 +9,76 @@ typedef struct Page{
   int altered;
 } Page;
 
+// Global variables for arguments
+char* algorithm;
+char* inputFile;
+int pageSize;
+int memorySize;
+int debug;
+
+void printUsage() {
+  fprintf(stderr, "Usage: ./tp2virtual <algorithm> <input file> <page size> <memory size> <debug?>\n");
+  fprintf(stderr, "Algorithms: lru, 2a, fifo, random\n");
+  fprintf(stderr, "Input file: string\n");
+  fprintf(stderr, "Page size: positive integer, recommended range [2, 64]\n");
+  fprintf(stderr, "Memory size: positive integer, recommended range [128, 16384]\n");
+  fprintf(stderr, "Debug (optional): 0, 1\n");
+}
+
+void parseArgs(int argc, char* argv[]){
+  if (argc < 5){
+    printUsage();
+    exit(1);
+  }
+
+  if (strcmp(argv[1], "lru") != 0 && strcmp(argv[1], "2a") != 0 && strcmp(argv[1], "fifo") != 0 && strcmp(argv[1], "random") != 0) {
+    fprintf(stderr, "Invalid algorithm\n");
+    printUsage();
+    exit(1);
+  }
+
+  if (atoi(argv[3]) <= 0){
+    fprintf(stderr, "Invalid page size\n");
+    printUsage();
+    exit(1);
+  }
+
+  if (atoi(argv[4]) <= 0){
+    fprintf(stderr, "Invalid memory size\n");
+    printUsage();
+    exit(1);
+  }
+  
+  if (argc == 6){
+    if (strcmp(argv[5], "0") != 0 && strcmp(argv[5], "1") != 0){
+      fprintf(stderr, "Invalid debug value\n");
+      printUsage();
+      exit(1);
+    }
+    else{
+      debug = atoi(argv[5]);
+    }
+  }
+  else{
+    debug = 0;
+  }
+
+  algorithm = argv[1];
+  inputFile = argv[2];
+  pageSize = atoi(argv[3]);
+  memorySize = atoi(argv[4]);
+}
+
 int main(int argc, char* argv[]){
-  // Variables
-  char* algorithm = argv[1];
-  char* inputFile = argv[2];
-  int pageSize = atoi(argv[3]); 
-  int memorySize = atoi(argv[4]);
+  // Parse arguments
+  parseArgs(argc, argv);
 
   // Print variables
   printf("Algorithm: %s\n", algorithm);
   printf("Input file: %s\n", inputFile);
-  printf("Memory size: %d\n", memorySize);
-  printf("Page size: %d\n", pageSize);
+  printf("Memory size: %d KB\n", memorySize);
+  printf("Page size: %d KB\n", pageSize);
+  printf("Debug: %d\n", debug);
 
   Page* memory = malloc(sizeof(Page) * memorySize/pageSize);
 
